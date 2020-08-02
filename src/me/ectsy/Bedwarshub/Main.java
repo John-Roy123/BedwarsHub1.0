2973000b3b5cd8fee0a98c5a233ca5d7a275423b
ectsy <johnroy.tja@gmail.com>
2020-08-01 22:51:45 -0400

    Initial commit

---
+++ src/me/ectsy/Bedwarshub/Main.java
@@ -0,0 +1,7 @@
+package me.ectsy.Bedwarshub;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+}
---
+++ src/me/ectsy/Bedwarshub/manager/Manager.java
@@ -0,0 +1,6 @@
+package me.ectsy.Bedwarshub.manager;
+
+
+
+public class Manager {
+}
